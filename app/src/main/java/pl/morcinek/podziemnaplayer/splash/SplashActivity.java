a1b8a038d9c99d3e580f71116ac94d7353caa829
Tomasz Morcinek <tomasz@morcinek.co.uk>
2015-04-09 17:53:14 +0100

    Small refactoring in SplashActivity.

--- app/src/main/java/pl/morcinek/podziemnaplayer/splash/SplashActivity.java
+++ app/src/main/java/pl/morcinek/podziemnaplayer/splash/SplashActivity.java
@@ -1,13 +1,15 @@
-package pl.morcinek.podziemnaplayer;
+package pl.morcinek.podziemnaplayer.splash;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 
+import pl.morcinek.podziemnaplayer.R;
+
 
 public class SplashActivity extends ActionBarActivity implements Runnable {
 
-    public static final int SPLASH_DELAY_MILLIS = 1200;
+    public static final int SPLASH_DELAY_MILLIS = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
