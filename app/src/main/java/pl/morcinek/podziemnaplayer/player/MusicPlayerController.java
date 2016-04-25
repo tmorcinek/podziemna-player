551881ba282e8dac4ee00c0ed028a00c8c2fb441
Tomasz Morcinek <tomasz@morcinek.co.uk>
2016-04-25 15:14:11 +0700

    Updating gradle. Work in progress on Media Player.

---
+++ app/src/main/java/pl/morcinek/podziemnaplayer/player/MusicPlayerController.java
@@ -0,0 +1,14 @@
+package pl.morcinek.podziemnaplayer.player;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Copyright 2015 Tomasz Morcinek. All rights reserved.
+ */
+public class MusicPlayerController {
+
+    private Context context;
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+}
